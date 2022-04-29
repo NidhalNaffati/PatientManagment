7b5448f6823f68afa714834cd8f96776f087f9f7
NidhalNaffati <nidhalnaffati@gmail.com>
2022-04-29 12:17:17 +0100

    Set Author and Date

--- src/com/nidhal/Launcher.java
+++ src/com/nidhal/Launcher.java
@@ -1,12 +1,18 @@
 package com.nidhal;
 
+
+/**
+ * @author Nidhal Naffati
+ * Date : 03-April-2022
+ */
+
 import com.nidhal.view.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage)  {
         ViewFactory viewFactory = new ViewFactory();
         viewFactory.showLoginWindow();
     }
